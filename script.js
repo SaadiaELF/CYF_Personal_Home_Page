513cdbfa1654adb03e7fd66755c303529324bb01
Saadia El fekak <74792703+SaadiaELF@users.noreply.github.com>
2022-05-21 13:52:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,62 @@
-/* =================
-  TESTS, LOOK AT THESE
-  Reading tests will always help you discover your requirements.
-  You can make this window bigger. 
-   ===================
-*/
+// var total, numbers, rolls, item;
 
-const {
-  core: { test, expect, run },
-  prettify
-} = window.jestLite;
+// Describe this function...
+function display_the_rolls(total) {
+  if(--window.LoopTrap <= 0) throw "Infinite loop.";
+  let element_total = document.getElementById('total');
+  element_total.innerText = total;
+  let element_list = document.getElementById('list');
+  element_list.replaceChildren();
+  rolls.forEach((item) => {
+    let new_li = document.createElement('li');
+    new_li.innerText = item;
 
-/* =================
-  FIND ELEMENTS
-  These are all the elements we will look for.
-   ===================
-*/
-const getHeader = document.querySelectorAll("header"),
-  getH1 = document.querySelectorAll("h1"),
-  getSiteHeader = document.querySelectorAll(".c-site-header"),
-  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
-  getMain = document.querySelectorAll("main"),
-  getFooter = document.querySelectorAll("footer"),
-  getSiteFooter = document.querySelectorAll(".c-site-footer"),
-  getIFrame = document.querySelectorAll("iframe"),
-  getImage = document.querySelectorAll("img"),
-  getWords = document.body.innerText;
+    element_list.appendChild(new_li);
+  });
+  if (total == 11) {
+    let element_info = document.getElementById('info');
+    element_info.innerText = 'You won!';
+  } else if (total > 11) {
+    let element_info2 = document.getElementById('info');
+    element_info2.innerText = 'You lost!';
+  }
+}
 
-/* =================
-   ASSERTIONS 
-   These are the things we check are true about your page.
-   Read and update your HTML to discover the requirements.
-   The tests will run every time you update your code.
-   ===================
-*/
-test("There is at least one header element", () => {
-  expect(getHeader.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one h1", () => {
-  expect(getH1.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one header element with the class c-site-header", () => {
-  expect(getSiteHeader.length).toBe(1);
-});
-test("There is a nav element with an aria-label of Main Site Links.", () => {
-  expect(getAria.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one main element", () => {
-  expect(getMain.length).toBe(1);
-});
-test("There is at least one footer element", () => {
-  expect(getFooter.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one footer element with the class c-site-footer", () => {
-  expect(getSiteFooter.length).toBe(1);
-});
-test("There is embedded video", () => {
-  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one image", () => {
-  expect(getImage.length).toBeGreaterThanOrEqual(1);
-});
-test("There are at least 500 words on the page", () => {
-  expect(getWords.length).toBeGreaterThanOrEqual(500);
+function randomInt(n) {
+  // Return a random number from in [0, n[
+  return Math.floor(Math.random()*n);
+}
+
+function randomMember(arr) {
+  // Return a random member of the array
+  return arr[randomInt(arr.length)]
+}
+
+
+numbers = [1, 2, 3, 4, 5, 6];
+rolls = [];
+
+rolls.reduce((a,b) => a+b, 0);
+
+
+document.getElementById('button_roll').addEventListener('click', (event) => {
+  rolls.unshift(randomMember(numbers));
+  total = rolls.reduce((a,b) => a+b, 0);
+  display_the_rolls(total);
+
 });
 
-const console = document.getElementById("tests");
-prettify.toHTML(run(), console);
+document.getElementById('button_remove').addEventListener('click', (event) => {
+  if (rolls != 0) {
+    rolls.pop();
+    display_the_rolls(total);
+  }
+
+});
+
+document.getElementById('button_restart').addEventListener('click', (event) => {
+  rolls = [];
+  total = 0;
+  display_the_rolls(total);
+
+});
